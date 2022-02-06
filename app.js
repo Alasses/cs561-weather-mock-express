c3f6b2268238a75d426bce222b492257f864d798
Michael Wang <wangshiyao96@gmail.com>
2022-02-06 13:12:39 -0800

    Added swagger doc and some expansion
    - Added swagger doc to src directory
    - Fixed express server to make it more consistent

--- app.js
+++ app.js
@@ -46,6 +46,7 @@ app.get("/v1/hello", verifyAuth, (req, res) => {
 app.get("/v1/weather", verifyAuth, (req, res) => {
 
   console.log("Verifying Authentication");
+  console.log(req.authorization)
 
   jwt.verify(req.token, 'secretkey', (err, authData) => {
     if(err)
@@ -70,20 +71,32 @@ app.get("/v1/weather", verifyAuth, (req, res) => {
 });
 
 app.post("/v1/auth", (req, res) => {
+
+  console.log("Receiving authentication request")
+
   if(req.body)
   {
     if(req.body.user && authUser[req.body.user] == req.body.password)
     {
+      console.log("Authentication Succeed\n")
+
       jwt.sign(req.body, 'secretkey', { expiresIn: 60 * 30 }, 
         (err, token) => {
-          res.json({
-            'AuthToken': token
-          });
+          
+        res.status(200);
+        res.header("Content-Type", 'application/json');
+        res.send({
+          'auth': token
+        });
+
         }
       );
     }
     else
     {
+      console.log("Authentication Failed")
+      console.log(req.body)
+
       res.status(403);
       res.header("Content-Type", "text/plain");
       res.send("User Not Found!");
@@ -102,7 +115,8 @@ function verifyAuth(req, res, next){
 
   if(bearerHeader != 'undefined')
   {
-    const bearerToken = bearerHeader;
+    const bearer = bearerHeader.split(' ');
+    const bearerToken = bearer[1];
     req.token = bearerToken;
     next();
   }
